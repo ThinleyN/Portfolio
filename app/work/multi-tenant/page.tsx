cb405da743e703b111c7fc3618474bc771233894
Thinley <thinley@Thinleys-MacBook-Air.local>
2026-01-06 23:16:58 +0000

    content: work stub pages

---
+++ app/work/multi-tenant/page.tsx
@@ -0,0 +1,13 @@
+export default function MuliTenantLMSSystem() {
+  return (
+    <div className="container m-auto">
+        <h2>Context</h2>
+        <h2>Problem</h2>
+        <h2>Constraints</h2>
+        <h2>Key Decisions</h2>
+        <h2>Architecture (high-level)</h2>
+        <h2>Outcome</h2>
+        <h2>What I’d Do Differently</h2>
+    </div>
+  );
+}
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,6 +2,15 @@ export default function About() {
     return (
       <section className="container m-auto">
           This is the work page.
+
+          <ul>
+            <li>
+              <a href="/multi-tenant">Multi tenant LMS platform</a>
+            </li>
+            <li>
+              <a href="/flagship-sass">Flagship SASS</a>
+            </li>
+          </ul>
       </section>
     );
   }
